0cae8e1fbd8e2832c875f2a2d56b82ffd9f6f623
Jacob <jayhoukes@gmail.com>
2025-05-16 12:40:32 +0200

    reorganizing into packages, refactoring

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from storage_json import StorageJson
+from storage.storage_json import StorageJson
 from movie_app import MovieApp
 
 
 def main():
     """This function initializes the movie app with JSON storage and starts the main loop."""
-    storage = StorageJson("data.json")
+    storage = StorageJson("data/data.json")
     app = MovieApp(storage)
     app.run()
 
